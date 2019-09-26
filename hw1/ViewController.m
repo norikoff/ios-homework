791024d01c12c0d1597ecc3571577148f1a42988
norikoff <yuranorikov@gmail.com>
2019-09-26 22:19:15 +0300

    hw done

--- hw1/ViewController.m
+++ hw1/ViewController.m
@@ -15,8 +15,87 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
+    //    [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    [self task1];
+    [self task2];
+}
+
+
+/**
+ Первое задание
+ */
+-(void) task1{
+    NSMutableArray *myIntegers = [NSMutableArray array];
+    [myIntegers addObject:[NSNumber numberWithInteger:24]];
+    [myIntegers addObject:[NSNumber numberWithInteger:6]];
+    [myIntegers addObject:[NSNumber numberWithInteger:32]];
+    [myIntegers addObject:[NSNumber numberWithInteger:3]];
+    [myIntegers addObject:[NSNumber numberWithInteger:81]];
+    //sort ascending
+    myIntegers = [NSMutableArray arrayWithArray:[myIntegers sortedArrayUsingSelector: @selector(compare:)]];
+    NSLog(@"sort ascending");
+    for (NSNumber *num in myIntegers) {
+        NSLog(@"%@",num);
+    }
+    NSLog(@"----------------------------------------------");
+    NSMutableArray *moreThanTwenty = [NSMutableArray array];
+    NSMutableArray *multipleOfThree = [NSMutableArray array];
+    NSLog(@"filter number more then 20");
+    for (NSNumber *num in myIntegers) {
+        if ([num integerValue]>20) {
+            [moreThanTwenty addObject: num];
+            NSLog(@"%@",num);
+        }
+    }
+    NSLog(@"----------------------------------------------");
+    NSLog(@"filter number multiple of three");
+    for (NSNumber *num in myIntegers) {
+        if ([num integerValue]%3==0) {
+            [moreThanTwenty addObject: num];
+            NSLog(@"%@",num);
+        }
+    }
+    NSLog(@"----------------------------------------------");
+    
+    [moreThanTwenty addObjectsFromArray:multipleOfThree];
+    [myIntegers addObjectsFromArray:moreThanTwenty];
+    NSLog(@"collect arrays");
+    for (NSNumber *num in myIntegers) {
+        NSLog(@"%@",num);
+    }
+    NSLog(@"----------------------------------------------");
+    myIntegers = [NSMutableArray arrayWithArray:[myIntegers sortedArrayUsingSelector: @selector(compare:)]];
+    myIntegers = myIntegers.reverseObjectEnumerator;
+    
+    NSLog(@"arrays sort descending");
+    for (NSNumber *num in myIntegers) {
+        NSLog(@"%@",num);
+    }
+    NSLog(@"----------------------------------------------");
+}
+
+
+/**
+ второе задание
+ */
+-(void) task2{
+    NSArray *arr = @[@"cataclism",@"catepillar",@"cat",@"teapot",@"truncate"];
+    NSString *str = @"cat";
+    //filer
+    NSPredicate* predicate = [NSPredicate predicateWithFormat:@"SELF BEGINSWITH %@", str];
+    NSArray* filteredData = [arr filteredArrayUsingPredicate:predicate];
+    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+    
+    //show filtered array
+    for (NSString *s in filteredData) {
+        [dict setObject: @(s.length) forKey:s];
+        NSLog(@"%@",s);
+    }
+    //dictionary str-length
+    NSLog(@"%@",dict);
+    
 }
 
 
